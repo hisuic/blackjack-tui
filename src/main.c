741d18c3f9a1eeca3211ffb77b202b15145d3a4d
hisui <henryharlay@gmail.com>
2026-02-01 20:35:43 +0900

    feat: Implement player turn with hit and stand

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "game.h"
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 void cleanup(CardCollection *deck, CardCollection *player_hand, CardCollection *dealer_hand) {
     free(deck->cards);
@@ -28,11 +29,39 @@ int main() {
     int player_score = calculate_score(&player_hand);
     int dealer_score = calculate_score(&dealer_hand);
 
-    render_game(&player_hand, &dealer_hand, player_score, dealer_score, "Welcome to Blackjack!", true);
+    char message[100] = "Your turn. (h) Hit, (s) Stand.";
+    bool game_over = false;
+    bool player_turn = true;
 
-    get_input(); // Wait for user to press a key
+    while (!game_over) {
+        render_game(&player_hand, &dealer_hand, player_score, dealer_score, message, player_turn);
+        int ch = get_input();
+
+        if (player_turn) {
+            if (ch == 'h') {
+                deal_card(&deck, &player_hand);
+                player_score = calculate_score(&player_hand);
+                if (player_score > 21) {
+                    strcpy(message, "You busted! Dealer wins.");
+                    player_turn = false;
+                }
+            } else if (ch == 's') {
+                player_turn = false;
+                strcpy(message, "Dealer's turn.");
+            } else if (ch == 'q') {
+                game_over = true;
+                strcpy(message, "Quitting...");
+            }
+        }
+        
+        // Simple exit for now after player turn
+        if (!player_turn) {
+            // Dealer logic will go here
+            get_input(); // Wait for keypress before exiting
+            game_over = true;
+        }
+    }
 
     cleanup(&deck, &player_hand, &dealer_hand);
-
     return 0;
 }
